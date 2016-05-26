5092392783721fa941b7121a6b4d102149613f86
Luis Santos (77900) <luis.d.santos@tecnico.ulisboa.pt>
2016-05-26 14:41:50 +0100

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,3 +1,13 @@
-sudo add-apt-repository ppa:numix/ppa
-sudo apt-get update
-sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+DOWNLOAD=apache-maven-3.3.9-bin.tar.gz
+LINK=http://mirrors.fe.up.pt/pub/apache/maven/maven-3/3.3.9/binaries/$DOWNLOAD
+DIR=temp_install_x
+
+OPT=/opt
+
+cd ~
+rm -rf $DIR
+mkdir $DIR
+cd $DIR
+wget $LINK
+tar vxjf $DOWNLOAD
+rm $DOWNLOAD
