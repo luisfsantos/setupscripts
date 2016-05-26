ad678230c802816a400b373eb5db8b1e3042b17b
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-05-26 16:16:31 +0100

    adding some repos

---
+++ albert.sh
@@ -0,0 +1,4 @@
+#https://github.com/ManuelSchneid3r/albert/wiki
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install albert
---
+++ gnome-do.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:do-core/ppa
+sudo apt-get update
+sudo apt-get install gnome-do
