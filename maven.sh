159e721059f7c7d440d354f12db9499f804341df
Luis Santos (77900) <luis.d.santos@tecnico.ulisboa.pt>
2016-05-26 14:58:30 +0100

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,10 +1,8 @@
 DOWNLOAD=apache-maven-3.3.9-bin.tar.gz
 LINK=http://mirrors.fe.up.pt/pub/apache/maven/maven-3/3.3.9/binaries/$DOWNLOAD
-DIR=temp_install_x
-
+DIR=~/temp_install_x
 OPT=/opt
 
-cd ~
 rm -rf $DIR
 mkdir $DIR
 cd $DIR
