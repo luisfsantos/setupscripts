b56e000f71db54a49ee9d65a572dd54d9cbfe418
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-05-26 15:36:12 +0100

    fix

--- java.sh
+++ java.sh
@@ -1,3 +1,4 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
--- maven.sh
+++ maven.sh
@@ -2,10 +2,19 @@ DOWNLOAD=apache-maven-3.3.9-bin.tar.gz
 LINK=http://mirrors.fe.up.pt/pub/apache/maven/maven-3/3.3.9/binaries/$DOWNLOAD
 DIR=~/temp_install_x
 OPT=/opt
+EXEC=apache-maven-3.3.9
+NEWEXEC=maven
 
 rm -rf $DIR
 mkdir $DIR
 cd $DIR
 wget $LINK
-tar vxjf $DOWNLOAD
+tar -xvzf $DOWNLOAD
 rm $DOWNLOAD
+sudo rm -rf $OPT/$NEWEXEC
+sudo mv $EXEC/ $OPT/$NEWEXEC
+printf '\n %s\n%s\n' 'export M2_HOME=/opt/maven' 'export PATH=$PATH:$M2_HOME/bin' >> ~/.bashrc
+rm -rf $DIR
+
+
+
